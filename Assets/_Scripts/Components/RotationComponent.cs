306d5d284f692738fe22cb7f1e818336e2fc1f01
redd096 <martin.babino96@gmail.com>
2023-04-10 13:01:45 +0200

    player can move and rotate

---
+++ Assets/_Scripts/Components/RotationComponent.cs
@@ -0,0 +1,96 @@
+using redd096;
+using UnityEngine;
+using redd096.Attributes;
+
+public class RotationComponent : MonoBehaviour
+{
+    [Header("Rotation")]
+    [SerializeField] float rotationSpeed = 10;
+
+    [Header("Necessary Components (by default get from this gameObject)")]
+    [SerializeField] Rigidbody rb = default;
+
+    [Header("DEBUG")]
+    [ReadOnly] public Vector3 AimDirectionInput = Vector3.right;            //when aim, set it with only direction (used to know where this object is aiming)
+    [ReadOnly] public Vector3 AimPositionNotNormalized = Vector3.right;     //when aim, set it without normalize (used to set crosshair on screen - to know mouse position or analog inclination)
+    [SerializeField] ShowDebugRedd096 showPositionNotNormalized = Color.red;
+    [SerializeField] ShowDebugRedd096 showDirectionInput = Color.cyan;
+
+    void OnDrawGizmos()
+    {
+        //draw sphere to see where is aiming
+        if (showPositionNotNormalized)
+        {
+            Gizmos.color = showPositionNotNormalized.ColorDebug;
+            Gizmos.DrawWireSphere(AimPositionNotNormalized, 1);
+        }
+        if (showDirectionInput)
+        {
+            Gizmos.color = showDirectionInput.ColorDebug;
+            Gizmos.DrawWireSphere(transform.position + AimDirectionInput, 1);
+        }
+        Gizmos.color = Color.white;
+    }
+
+    private void FixedUpdate()
+    {
+        //start only if there are all necessary components
+        if (CheckComponents() == false)
+            return;
+
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        Quaternion lookRotation = Quaternion.LookRotation(AimDirectionInput, Vector3.up);
+
+        rb.rotation = Quaternion.Lerp(rb.rotation, lookRotation, Time.fixedDeltaTime * rotationSpeed);
+    }
+
+    #region private API
+
+    bool CheckComponents()
+    {
+        //check if have components
+        if (rb == null)
+            rb = GetComponent<Rigidbody>();
+
+        //if movement mode is rigidbody, be sure to have a rigidbody
+        if (rb == null)
+        {
+            Debug.LogWarning("Miss Rigidbody on " + name);
+            return false;
+        }
+
+        return true;
+    }
+
+    #endregion
+
+    #region public API
+
+    /// <summary>
+    /// Set aim in direction
+    /// </summary>
+    /// <param name="aimDirection"></param>
+    public void AimInDirection(Vector3 aimDirection)
+    {
+        //set direction aim
+        AimPositionNotNormalized = transform.position + aimDirection;
+        AimDirectionInput = aimDirection.normalized;
+    }
+
+    /// <summary>
+    /// Set aim at position
+    /// </summary>
+    /// <param name="aimPosition"></param>
+    public void AimAt(Vector3 aimPosition)
+    {
+        //set direction aim
+        AimPositionNotNormalized = aimPosition;
+        AimDirectionInput = (aimPosition - transform.position).normalized;
+    }
+
+    #endregion
+}
---
+++ Assets/_Scripts/StateMachineTasks/Actions/AimByInput.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using redd096.Attributes;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+using redd096.StateMachine.StateMachineRedd096;
+
+public class AimByInput : ActionTask
+{
+#if ENABLE_INPUT_SYSTEM
+    [Header("Necessary Components - default get in parent")]
+    [SerializeField] RotationComponent aimComponent = default;
+    [SerializeField] PlayerInput playerInput = default;
+
+    [Header("Aim")]
+    [SerializeField] string inputName = "Move";
+    [SerializeField] bool resetWhenReleaseAnalogInput = false;
+
+    Vector2 inputValue;
+    Vector2 lastAnalogSavedValue = Vector2.right;
+
+    protected override void OnInitTask()
+    {
+        base.OnInitTask();
+
+        //set references
+        if (aimComponent == null) aimComponent = GetStateMachineComponent<RotationComponent>();
+        if (playerInput == null) playerInput = GetStateMachineComponent<PlayerInput>();
+
+        lastAnalogSavedValue = new Vector2(transformTask.forward.x, transformTask.forward.z);
+
+        //show warnings if not found
+        if (playerInput && playerInput.actions == null)
+            Debug.LogWarning("Miss Actions on PlayerInput on " + StateMachine);
+    }
+
+    public override void OnUpdateTask()
+    {
+        base.OnUpdateTask();
+
+        if (aimComponent == null || playerInput == null || playerInput.actions == null)
+            return;
+
+        //get input
+        inputValue = playerInput.actions.FindAction(inputName).ReadValue<Vector2>();
+
+        //check if moving analog or reset input when released
+        if (inputValue != Vector2.zero || resetWhenReleaseAnalogInput)
+        {
+            aimComponent.AimInDirection(new Vector3(inputValue.x, 0, inputValue.y));
+            lastAnalogSavedValue = inputValue;  //save input
+        }
+        //else show last saved input
+        else
+        {
+            aimComponent.AimInDirection(new Vector3(lastAnalogSavedValue.x, 0, lastAnalogSavedValue.y));
+        }
+    }
+#else
+    [HelpBox("This works only with new unity input system", HelpBoxAttribute.EMessageType.Error)]
+    public string Error = "It works only with new unity input system";
+#endif
+}
