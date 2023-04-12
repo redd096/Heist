5a2613abca03b5a496dd8234a3b4f681e1271786
redd096 <martin.babino96@gmail.com>
2023-04-12 19:18:28 +0200

    edit wannabe gate

--- Assets/_Scripts/WannabeGate.cs
+++ Assets/_Scripts/WannabeGate.cs
@@ -4,27 +4,71 @@ using UnityEngine;
 
 public class WannabeGate : MonoBehaviour
 {
+    [Header("Ignore objects with these tags or without it?")]
+    [SerializeField] bool ignoreObjectsWithTheseTags = true;
+    [SerializeField] List<string> tags = new List<string>() { "TriggerZone" };
+
+    [Header("Open when something is in trigger, or close?")]
+    [SerializeField] bool openWhenInTrigger = true;
     public GameObject gate;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    List<Collider> collidersInTrigger = new List<Collider>();
+    List<WannabeGate> wannabeGates = new List<WannabeGate>();
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if(other.tag != "TriggerZone")
+        //add to the list every other pressure plate that activate this gate
+        foreach (WannabeGate wannabeGate in FindObjectsOfType<WannabeGate>())
         {
-            gate.SetActive(false);
+            if (wannabeGate.gate == gate)
+                wannabeGates.Add(wannabeGate);
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //add to the list and try activate gate
+        if (IsCorrectObject(other))
+        {
+            if (collidersInTrigger.Contains(other) == false) collidersInTrigger.Add(other);
+            TryActivate();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //remove from the list and try activate gate
+        if (IsCorrectObject(other))
+        {
+            if (collidersInTrigger.Contains(other)) collidersInTrigger.Remove(other);
+            TryActivate();
+        }
+    }
+
+    bool IsCorrectObject(Collider other)
+    {
+        if (ignoreObjectsWithTheseTags)
+            return tags.Contains(other.tag) == false;   //is correct if it's tag is NOT in the list
         else
-            gate.SetActive(true);
+            return tags.Contains(other.tag);            //is correct if it's tag is in the list
+    }
+
+    bool CheckThereIsSomethingInTrigger()
+    {
+        //check at least one pressure plate has something on it
+        foreach (WannabeGate wannabeGate in wannabeGates)
+            if (wannabeGate.collidersInTrigger.Count > 0)
+                return true;
+
+        return false;
+    }
+
+    private void TryActivate()
+    {
+        //check if something is on trigger. False to open, True to close
+        if(CheckThereIsSomethingInTrigger())
+            gate.SetActive(openWhenInTrigger == false);
+        else
+            gate.SetActive(openWhenInTrigger);
     }
 }
