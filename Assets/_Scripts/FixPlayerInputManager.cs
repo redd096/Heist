d0ed8f34d72abc1eee5edaf60da6411c9e2a2ecb
redd096 <martin.babino96@gmail.com>
2023-04-14 12:27:15 +0200

    fix player input manager

--- Assets/_Scripts/FixPlayerInputManager.cs
+++ Assets/_Scripts/FixPlayerInputManager.cs
@@ -3,18 +3,20 @@ using UnityEngine.InputSystem;
 
 public class FixPlayerInputManager : MonoBehaviour
 {
+    enum EState { waitingToBeInitialized, CanBeUsed, Destroyed }
+    EState currentState = EState.waitingToBeInitialized;
+
     private void Start()
     {
         //in normal game, we have player input manager only in the local lobby, without this script
         //but in gameplay scenes, we have it with this script, just to test rapidly
-        if (GameManager.instance && GameManager.levelManager && PlayerInputManager.instance == this && NetworkManager.instance == null)
+
+        if (Check())
         {
-            //if we are in gameplay scene, and this is the instance. Then this is the first scene, so we are testing in editor
             PlayerInputManager.instance.EnableJoining();
         }
         else
         {
-            //else, we are playing normally, and we don't need this in scene
             Destroy(gameObject);
         }
     }
@@ -25,13 +27,34 @@ public class FixPlayerInputManager : MonoBehaviour
 
         //call if the player is istantiated after the countdown.
         //If the countodown is still running, this do nothing and the player will be activated normally
-        if (GameManager.instance && GameManager.levelManager && PlayerInputManager.instance == this && NetworkManager.instance == null)
+        if (Check())
             GameManager.levelManager.TryActivatePlayer(playerInput.GetComponent<PlayerController>());
     }
 
     public void OnPlayerLeft(PlayerInput playerInput)
     {
-        if (GameManager.instance && GameManager.levelManager && PlayerInputManager.instance == this && NetworkManager.instance == null)
+        if (Check())
             Destroy(playerInput.gameObject);
+    }
+
+    private bool Check()
+    {
+        if (currentState == EState.CanBeUsed)
+            return true;
+        if (currentState == EState.Destroyed)
+            return false;
+
+        //if we are in gameplay scene, and we are not online and there aren't players in scene, then this is the first scene, so we are testing in editor
+        if (GameManager.instance && GameManager.levelManager && NetworkManager.instance == null && FindObjectsOfType<PlayerController>().Length <= 0)
+        {
+            currentState = EState.CanBeUsed;
+            return true;
+        }
+        //else, we are playing normally, and we don't need this in scene
+        else
+        {
+            currentState = EState.Destroyed;
+            return false;
+        }
     }
 }
