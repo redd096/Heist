53e478faee2fbc8a382827d3b4a9a7187375334a
redd096 <martin.babino96@gmail.com>
2023-04-15 19:36:21 +0200

    unlock level

--- Assets/_Scripts/Managers/SelectLevelManager.cs
+++ Assets/_Scripts/Managers/SelectLevelManager.cs
@@ -1,4 +1,3 @@
-using Fusion;
 using redd096;
 using redd096.Attributes;
 using UnityEngine;
@@ -10,12 +9,17 @@ public class SelectLevelManager : MonoBehaviour
     [Scene][SerializeField] string backSceneOffline = "LocalMenu";
     [SerializeField] LevelButtonStruct[] levelButtons = default;
 
+    [Button(ButtonAttribute.EEnableType.PlayMode)] void UnlockAllLevels() { foreach (var v in levelButtons) v.button.interactable = true; }
+
     private void Start()
     {
         for (int i = 0; i < levelButtons.Length; i++)
         {
             string s = levelButtons[i].level;
             levelButtons[i].button.onClick.AddListener(() => LoadLevel(s));
+
+            int scoreInLevelToCheck = SaveManager.PlayerPrefsFWMV.GetInt(GetHighScore.HIGHSCORE_SAVE, levelButtons[i].levelToCheckScoreForUnlock, 0);
+            levelButtons[i].button.interactable = scoreInLevelToCheck >= levelButtons[i].minScoreToUnlock;
         }
     }
 
@@ -37,5 +41,7 @@ public class SelectLevelManager : MonoBehaviour
     {
         public Button button;
         [Scene] public string level;
+        [Min(0)] public int minScoreToUnlock;
+        [Scene] public string levelToCheckScoreForUnlock;
     }
 }
