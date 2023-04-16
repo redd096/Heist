e05ed365184b18f71401698bf5a32579a69db4e8
redd096 <martin.babino96@gmail.com>
2023-04-16 16:06:14 +0200

    scene changer animation

---
+++ Assets/_Scripts/redd096Utilities/redd096/Singletons/SceneChangerAnimation.cs
@@ -0,0 +1,230 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using redd096.Attributes;
+using UnityEngine.UI;
+
+//Create a material with unity shader UI/Default and set:
+//- Stencil Comparison      6
+//- Stencil ID              1
+//- Stencil Operation       0
+//- Stencil Write Mask      0
+//- Stencil Read Mask       1
+//- Color Mask              15
+//- Use Alpha Clip          false
+
+//create a canvas and attach this script.
+//Set SortOrder to a big number to be sure is on top of everything
+
+//Create an Image inside the canvas (this will be the showed sprite)
+//- Set the sprite we want to show
+//- Attach Mask
+//- Set Show Mask Graphic to false
+
+//Create another Image, child of the first one
+//- Set Color to black
+//- Set Maskable to false
+//- Set as Material the one created before
+
+//NB you can disable the images object to not shown in editor scene.
+//Not the canvas if the script is on it, or it will not call Start.
+
+namespace redd096
+{
+    [AddComponentMenu("redd096/MonoBehaviours/Scene Changer Animation")]
+    public class SceneChangerAnimation : Singleton<SceneChangerAnimation>
+    {
+        [Header("Necessary components - default get from children")]
+        [SerializeField] CanvasScaler canvasScaler = default;
+        [SerializeField] Image mask = default;
+        [SerializeField] Image blackScreen = default;
+
+        [Header("Get random sprite - if empty, keep the one on the mask image")]
+        [SerializeField] Sprite[] randomSprites = default;
+
+        [Header("Fade In on Awake")]
+        [SerializeField] bool fadeInOnAwake = true;
+        [EnableIf("fadeInOnAwake")][SerializeField] float waitBeforeFadeOnAwake = 0.3f;
+
+        [Header("Animation")]
+        [SerializeField] float animDuration = 2f;
+        [Tooltip("Use realtime or deltaTime?")][SerializeField] bool useRealtimeInsteadOfDeltatime = true;
+
+        [Header("Fade use ScreenSize, need offset?")]
+        [SerializeField] Vector2 maskOffset = Vector2.zero;
+        [SerializeField] Vector2 blackScreenOffset = Vector2.zero;
+
+        Vector2 maskSizeFade => canvasScaler.referenceResolution + maskOffset;
+        Vector2 blackScreenSizeFade => canvasScaler.referenceResolution + blackScreenOffset;
+
+        EventSystem eventSystem;
+        Coroutine fadeCoroutine;
+        RectTransform maskRect;
+        RectTransform blackScreenRect;
+
+        //events
+        public System.Action onFadeInComplete { get; set; }
+
+        protected override void SetDefaults()
+        {
+            base.SetDefaults();
+
+            //get references
+            if (canvasScaler == null) canvasScaler = GetComponentInChildren<CanvasScaler>();
+            if (mask == null) mask = GetComponentInChildren<Image>();
+            if (blackScreen == null) blackScreen = mask.transform.GetChild(0).GetComponentInChildren<Image>();
+
+            if (canvasScaler == null && mask == null || blackScreen == null)
+            {
+                Debug.LogWarning($"Missing references on SceneChangerAnimation", gameObject);
+                return;
+            }
+
+            //get rect transform references
+            if (maskRect == null) maskRect = mask.GetComponent<RectTransform>();
+            if (blackScreenRect == null) blackScreenRect = blackScreen.GetComponent<RectTransform>();
+
+            //fade in when enter in scene
+            if (fadeInOnAwake)
+            {
+                //set size
+                maskRect.sizeDelta = Vector2.zero;
+                blackScreenRect.sizeDelta = blackScreenSizeFade;
+
+                //be sure objects are active
+                mask.gameObject.SetActive(true);
+                blackScreen.gameObject.SetActive(true);
+                canvasScaler.gameObject.SetActive(true);
+
+                //fade in after few seconds
+                Invoke(nameof(FadeIn), waitBeforeFadeOnAwake);
+            }
+            //deactivate images, to be sure the screen is clear
+            else
+            {
+                canvasScaler.gameObject.SetActive(false);
+            }
+        }
+
+        #region public API
+
+        /// <summary>
+        /// Do fade in
+        /// </summary>
+        public void FadeIn()
+        {
+            StartFade(true, OnFadeInComplete);
+        }
+
+        /// <summary>
+        /// Fade out, then move to next scene in build settings
+        /// </summary>
+        public void FadeOutNextScene()
+        {
+            StartFade(false, () => SceneLoader.instance.LoadNextScene());
+        }
+
+        /// <summary>
+        /// Fade out, then load passed scene
+        /// </summary>
+        /// <param name="scene"></param>
+        public void FadeOutLoadScene(string scene)
+        {
+            StartFade(false, () => SceneLoader.instance.LoadScene(scene));
+        }
+
+        /// <summary>
+        /// Fade out, then reload this scene
+        /// </summary>
+        public void FadeOutReloadScene()
+        {
+            StartFade(false, () => SceneLoader.instance.ReloadScene());
+        }
+
+        /// <summary>
+        /// Fade out, then exit from game
+        /// </summary>
+        public void ExitGame()
+        {
+            StartFade(false, () => SceneLoader.instance.ExitGame());
+        }
+
+        #endregion
+
+        #region private API
+
+        void StartFade(bool fadeIn, System.Action func)
+        {
+            if (mask == null || blackScreen == null)
+            {
+                Debug.LogWarning($"Missing references on SceneChangerAnimation", gameObject);
+                return;
+            }
+
+            //disable event system until finish fade
+            eventSystem = EventSystem.current;
+            if (eventSystem)
+                eventSystem.enabled = false;
+
+            //select random sprite
+            if (randomSprites != null && randomSprites.Length > 0)
+                mask.sprite = randomSprites[Random.Range(0, randomSprites.Length)];
+
+            //set size
+            maskRect.sizeDelta = fadeIn ? Vector2.zero : maskSizeFade;
+            blackScreenRect.sizeDelta = blackScreenSizeFade;
+
+            //be sure objects are active
+            mask.gameObject.SetActive(true);
+            blackScreen.gameObject.SetActive(true);
+            canvasScaler.gameObject.SetActive(true);
+
+            //start fade coroutine
+            if (fadeCoroutine != null)
+                StopCoroutine(fadeCoroutine);
+
+            fadeCoroutine = StartCoroutine(FadeCoroutine(fadeIn, func));
+        }
+
+        void OnFadeInComplete()
+        {
+            //re-enable event system
+            if (eventSystem)
+            {
+                eventSystem.enabled = true;
+
+                //set singleton if not setted, because unity remove it when disabled
+                if (EventSystem.current == null)
+                    EventSystem.current = eventSystem;
+
+            }
+
+            //deactivate images, to be sure the screen is clear
+            canvasScaler.gameObject.SetActive(false);
+
+            //call event
+            onFadeInComplete?.Invoke();
+        }
+
+        IEnumerator FadeCoroutine(bool fadeIn, System.Action func)
+        {
+            //set default vars
+            Vector2 startScale = maskRect.sizeDelta;
+
+            float delta = 0;
+            while (delta < 1)
+            {
+                yield return null;
+
+                //fade (with realtime or delta)
+                delta += (useRealtimeInsteadOfDeltatime ? Time.unscaledDeltaTime : Time.deltaTime) / animDuration;
+                if (mask) maskRect.sizeDelta = Vector2.Lerp(startScale, fadeIn ? maskSizeFade : Vector2.zero, delta);
+            }
+
+            //call function on complete
+            func?.Invoke();
+        }
+
+        #endregion
+    }
+}
